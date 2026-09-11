c0d814ad3e8a8b17ed5d17cb79dc29629e82dd65
SamridhiRise4 <samriddhishukla77@gmail.com>
2026-09-11 20:59:39 +0530

    Added

---
+++ Events.js
@@ -0,0 +1,9 @@
+let bt = document.querySelector("#bt");
+bt.onclick = ()=>{
+    console.log("Button was Clicked");
+}
+
+let div = document.querySelector("#ct");
+div.onmouseover = ()=>{
+    console.log("Hiii!!");
+};
